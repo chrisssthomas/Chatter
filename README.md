d3082975bf5a56d71d695d9297bbeb6bc7b0b7ab
Christopher Thomas <chrissssthomas@gmail.com>
2021-03-27 22:57:51 +0000

    Dockerizing the application, more docs to come, deployment next

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Twitter clone built with Laravel and Blade.
+
+# `docker-compose up --build`
+
+# Set .env variables
+
+# Running in Docker, visit http://localhost:8080/
--- app/Followable.php
+++ app/Followable.php
@@ -18,10 +18,10 @@ trait Followable
     }
 
 
-    public function follows(): BelongsToMany
+    public function follows()
     {
         return $this->belongsToMany(
-            User::class,
+            'App\User',
             'follows',
             'user_id',
             'following_user_id',
